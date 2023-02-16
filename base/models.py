fa6f9f690919d8ee98157ea18bb334f91b682bd6
oleksandr <o.l@smoothr.de>
2023-02-16 12:56:00 +0200

    time fix

--- base/models.py
+++ base/models.py
@@ -1,5 +1,11 @@
+from datetime import datetime, timedelta
+
 from django.db import models
 from django.contrib.auth.models import User
+
+
+def EET_time():
+    return datetime.now() + timedelta(hours=2)
 
 
 class Task(models.Model):
@@ -7,11 +13,10 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
-    create = models.DateTimeField(auto_now_add=True)
+    create = models.DateTimeField(default=EET_time)
 
     def __str__(self):
         return self.title
 
     class Meta:
         ordering = ['complete', '-create']
-
